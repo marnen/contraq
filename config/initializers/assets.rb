b5b09f4e95b75f019d4c106243253c8068aae637
Marnen Laibow-Koser <marnen@marnen.org>
2018-01-18 09:14:46 -0500

    Add normalize-sass to assets explicitly. [#20]

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -4,7 +4,8 @@
 Rails.application.config.assets.version = '1.0'
 
 # Add additional assets to the asset load path
-Rails.application.config.assets.paths << Rails.root.join('node_modules')
+normalize_path = Bundler.rubygems.find_name('normalize-scss').first.full_gem_path
+Rails.application.config.assets.paths += [Rails.root.join('node_modules'), File.join(normalize_path, 'sass')]
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
