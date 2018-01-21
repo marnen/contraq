0a67ba63f65da943eeacf714d1a8509e6d35f58c
Marnen Laibow-Koser <marnen@marnen.org>
2018-01-20 23:04:42 -0500

    Fix bug in factory.

--- spec/factories/gig.rb
+++ spec/factories/gig.rb
@@ -3,7 +3,7 @@ FactoryGirl.define do
     user
     name { Faker::Lorem.sentence }
     start_time { Faker::Time.forward 100, :evening }
-    end_time { start_time + 2.hours }
+    end_time { start_time.to_datetime + 2.hours }
     venue { Faker::Company.name }
     street { Faker::Address.street_address }
     city { Faker::Address.city }
