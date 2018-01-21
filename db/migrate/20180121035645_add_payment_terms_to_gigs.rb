cee1b6938cd9c84ea87ec173dd8a775eb0b24710
Marnen Laibow-Koser <marnen@marnen.org>
2018-01-20 23:15:39 -0500

    Rename migration. [#3]

--- db/migrate/20180121035645_add_payment_terms_to_gigs.rb
+++ db/migrate/20180121035645_add_payment_terms_to_gigs.rb
@@ -1,4 +1,4 @@
-class AddPaymentTermsToGig < ActiveRecord::Migration[5.1]
+class AddPaymentTermsToGigs < ActiveRecord::Migration[5.1]
   def change
     add_column :gigs, :amount_due, :decimal, precision: 8, scale: 2
     add_column :gigs, :terms, :integer, limit: 2
