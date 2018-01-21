af3c0fefb476e34fabb7f33001b48c0e988823b7
Marnen Laibow-Koser <marnen@marnen.org>
2018-01-21 01:27:06 -0500

    Refactor setup. [#3]

--- spec/decorators/gig_decorator_spec.rb
+++ spec/decorators/gig_decorator_spec.rb
@@ -6,41 +6,45 @@ RSpec.describe GigDecorator do
     expect(subject).to be_a_kind_of ApplicationDecorator
   end
 
-  describe '#location' do
+  context 'decorator methods' do
+    let(:gig) { FactoryGirl.create :gig, params, &block_params }
     let(:params) { {} }
-    let(:gig) { FactoryGirl.create :gig, params }
+    let(:block_params) { ->(gig) { } }
+    let(:decorator) { gig.decorate }
 
-    subject { gig.decorate.location }
+    describe '#location' do
+      subject { decorator.location }
 
-    context 'both city and state present' do
-      it 'returns the city and state, comma-separated' do
-        expect(subject).to be == "#{gig.city}, #{gig.state}"
+      context 'both city and state present' do
+        it 'returns the city and state, comma-separated' do
+          expect(subject).to be == "#{gig.city}, #{gig.state}"
+        end
+      end
+
+      context 'no city' do
+        let(:params) { {city: nil} }
+
+        it 'returns the state only' do
+          expect(subject).to be == gig.state
+        end
+      end
+
+      context 'no state' do
+        let(:params) { {state: nil} }
+
+        it 'returns the city only' do
+          expect(subject).to be == gig.city
+        end
       end
     end
 
-    context 'no city' do
-      let(:params) { {city: nil} }
+    describe '#time_range' do
+      let(:block_params) { ->(gig) { gig.end_time = gig.start_time + rand(2..5).hours } }
+      let(:subject) { decorator.time_range }
 
-      it 'returns the state only' do
-        expect(subject).to be == gig.state
+      it 'returns the start and end times for the gig in human-readable format, joined by an en-dash' do
+        expect(subject).to be == [gig.start_time, gig.end_time].map {|time| time.strftime '%-d %b %Y %-l:%M %p' }.join('–')
       end
-    end
-
-    context 'no state' do
-      let(:params) { {state: nil} }
-
-      it 'returns the city only' do
-        expect(subject).to be == gig.city
-      end
-    end
-  end
-
-  describe '#time_range' do
-    it 'returns the start and end times for the gig in human-readable format, joined by an en-dash' do
-      gig = FactoryGirl.create(:gig) do |gig|
-        gig.end_time = gig.start_time + rand(2..5).hours
-      end.decorate
-      expect(gig.time_range).to be == [gig.start_time, gig.end_time].map {|time| time.strftime '%-d %b %Y %-l:%M %p' }.join('–')
     end
   end
 end
