9e3206bfc8e3164cdaf376c2a791f57e2180a7ff
Marnen Laibow-Koser <marnen@marnen.org>
2018-01-21 22:40:31 -0500

    Extract buttons to cell. [#3]

---
+++ app/cells/button_cell.rb
@@ -0,0 +1,26 @@
+class ButtonCell < Cell::ViewModel
+  include FontAwesome::Sass::Rails::ViewHelpers
+
+  attr_reader :css_class, :icon_name, :link_options, :text
+
+  def show
+    set_options_for_action!
+    render
+  end
+
+  private
+
+  def set_options_for_action!
+    action = options[:action]
+    @css_class = "#{action}-gig"
+    @text = _ "#{action.to_s.titleize} gig"
+    case action
+    when :edit
+      @icon_name = 'pencil'
+      @link_options = edit_gig_path(model)
+    when :new
+      @icon_name = 'plus-circle'
+      @link_options = {action: 'new'}
+    end
+  end
+end
