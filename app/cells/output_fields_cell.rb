de901f6f15b145a7394a8b6378b88dccbd05f5bf
Marnen Laibow-Koser <marnen@marnen.org>
2018-01-22 01:58:17 -0500

    Refactor. [#25]

--- app/cells/output_fields_cell.rb
+++ app/cells/output_fields_cell.rb
@@ -1,13 +1,9 @@
 class OutputFieldsCell < Cell::ViewModel
   class << self
-    def css_class(string = nil, &block)
-      block = string ? -> { string } : block
-      define_method :css_class, block
-    end
-
-    def fields(hash = nil, &block)
-      block = hash ? -> { hash } : block
-      define_method :fields, block
+    %i[css_class fields].each do |name|
+      define_method name do |value|
+        define_method name, -> { value }
+      end
     end
   end
 
