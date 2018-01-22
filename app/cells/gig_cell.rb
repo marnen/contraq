07cf8c4678b3813e671d0b6a3a491fc548eaabec
Marnen Laibow-Koser <marnen@marnen.org>
2018-01-22 01:15:46 -0500

    Remove duplication by nesting cells. [#25]

--- app/cells/gig_cell.rb
+++ app/cells/gig_cell.rb
@@ -6,4 +6,12 @@ class GigCell < Cell::ViewModel
   private
 
   property :name, :time_range, :location
+
+  def fields
+    %i[time_range location payment]
+  end
+
+  def payment
+    cell 'gig/payment', model
+  end
 end
