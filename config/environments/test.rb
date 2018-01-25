5f594461ce67e46706275a27c95ba73b0a409fa8
Marnen Laibow-Koser <marnen@marnen.org>
2018-01-24 22:27:44 -0500

    Fix locale configuration. [#38]

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -39,7 +39,4 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
-
-  default_locale = config.i18n.default_locale
-  config.i18n.available_locales = [default_locale, :en, :'en-US'].compact # see https://github.com/stympy/faker/issues/480 and https://github.com/stympy/faker/issues/266
 end
