cce607ab1cd91ec2c7bb251ead60be75f02a2eb3
Marnen Laibow-Koser <marnen@marnen.org>
2018-03-01 00:04:10 -0500

    Get a rudimentary user login feature working. [#70]

--- phoenix/contraq/config/test.exs
+++ phoenix/contraq/config/test.exs
@@ -4,7 +4,9 @@ use Mix.Config
 # you can enable the server option below.
 config :contraq, ContraqWeb.Endpoint,
   http: [port: 4001],
-  server: false
+  server: true # TODO: should really be false except for white_bread
+
+config :contraq, sql_sandbox: true
 
 # Print only warnings and errors during test
 config :logger, level: :warn
--- phoenix/contraq/features/contexts/default_context.exs
+++ phoenix/contraq/features/contexts/default_context.exs
@@ -1,9 +1,13 @@
 defmodule DefaultContext do
   use WhiteBread.Context
 
+  import_steps_from DefaultContext.AllSteps
+
+  {:ok, _} = Application.ensure_all_started(:faker)
+  Application.put_env(:wallaby, :base_url, ContraqWeb.Endpoint.url)
+  {:ok, _} = Application.ensure_all_started(:wallaby)
+
   feature_starting_state fn ->
-    {:ok, _} = Application.ensure_all_started(:faker)
-    {:ok, _} = Application.ensure_all_started(:wallaby)
     %{}
   end
 
@@ -11,11 +15,71 @@ defmodule DefaultContext do
     :ok = Ecto.Adapters.SQL.Sandbox.checkout(Contraq.Repo)
     Ecto.Adapters.SQL.Sandbox.mode(Contraq.Repo, {:shared, self()})
     metadata = Phoenix.Ecto.SQL.Sandbox.metadata_for(Contraq.Repo, self())
-    {:ok, session} = Wallaby.start_session metadata: metadata
+    {:ok, session} = Wallaby.start_session #metadata: metadata
     state |> put_in([:session], session)
   end
 
-  scenario_finalize fn _state ->
+  scenario_finalize fn _status, _state ->
     Ecto.Adapters.SQL.Sandbox.checkin(Contraq.Repo, [])
+  end
+end
+
+defmodule DefaultContext.AllSteps do
+  use Wallaby.DSL
+  use WhiteBread.Context
+
+  import ContraqWeb.Router.Helpers
+  alias ContraqWeb.Endpoint
+  alias Contraq.Factory
+
+  given_ "I am not logged in", fn %{session: session} = state ->
+    session |> visit(session_path Endpoint, :delete)
+    {:ok, state}
+  end
+
+  given_ ~r/^I am logged in with e-?mail "(?<email>[^"]+)" and password "(?<password>[^"]+)"$/,
+  fn %{session: session} = state, attributes ->
+    Factory.insert! :user, attributes
+    session |> visit(session_path Endpoint, :new) |> login_as(attributes)
+    {:ok, state}
+  end
+
+  given_ "the following user exists:", fn state, {:table_data, [attributes]} ->
+    Factory.insert! :user, attributes
+    {:ok, state}
+  end
+
+  when_ "I go to the login page", fn %{session: session} = state ->
+    session |> visit(session_path Endpoint, :new)
+    {:ok, state}
+  end
+
+  when_ ~r/^I log in with e-?mail "(?<email>[^"]+)" and password "(?<password>[^"]+)"$/,
+  fn %{session: session} = state, attributes ->
+    session |> login_as(attributes)
+    {:ok, state}
+  end
+
+  when_ "I log out", fn %{session: session} = state ->
+    session |> visit(session_path Endpoint, :delete)
+    {:ok, state}
+  end
+
+  then_ ~r/^I should be logged in as "(?<email>[^"]+)"$/,
+  fn %{session: session} = state, %{email: email} ->
+    assert session |> has_text?("Logged in as #{email}")
+    {:ok, state}
+  end
+
+  then_ "I should not be logged in", fn %{session: session} = state ->
+    refute session |> has_text?("Logged in as")
+    {:ok, state}
+  end
+
+  defp login_as(session, %{email: email, password: password}) do
+    session
+    |> fill_in(Query.text_field("Email"), with: email)
+    |> fill_in(Query.text_field("Password"), with: password)
+    |> click(Query.button "Sign In")
   end
 end
