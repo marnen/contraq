d5b141a40ae07336b0aeeb61e6baba8228aade83
Marnen Laibow-Koser <marnen@marnen.org>
2018-03-01 17:03:48 -0500

    Use sandbox repo. [#70]

--- phoenix/contraq/spec/spec_helper.exs
+++ phoenix/contraq/spec/spec_helper.exs
@@ -1,10 +1,11 @@
+Code.require_file("#{__DIR__}/phoenix_helper.exs")
+
 ESpec.configure fn(config) ->
-  config.before fn(tags) ->
-    {:shared, hello: :world, tags: tags}
+  config.before fn(_tags) ->
+    :ok = Ecto.Adapters.SQL.Sandbox.checkout(Contraq.Repo)
   end
 
   config.finally fn(_shared) ->
-    :ok
+    Ecto.Adapters.SQL.Sandbox.checkin(Contraq.Repo, [])
   end
 end
-Code.require_file("spec/phoenix_helper.exs")
