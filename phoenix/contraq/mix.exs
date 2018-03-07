d5583a142441a6dce57b832fae2ce9135c765a64
Marnen Laibow-Koser <marnen@marnen.org>
2018-03-07 03:07:25 -0500

    Warn for underspecified functions. [#70]

--- phoenix/contraq/mix.exs
+++ phoenix/contraq/mix.exs
@@ -12,7 +12,11 @@ defmodule Contraq.Mixfile do
       aliases: aliases(),
       deps: deps(),
       preferred_cli_env: [espec: :test, "white_bread.run": :test],
-      dialyzer: [plt_add_deps: :transitive, ignore_warnings: "dialyzer.ignore-warnings"]
+      dialyzer: [
+        flags: [:underspecs],
+        ignore_warnings: "dialyzer.ignore-warnings",
+        plt_add_deps: :transitive
+      ]
     ]
   end
 
