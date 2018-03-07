c312fd439319660eaa31b2ac43dc677c8e210c64
Marnen Laibow-Koser <marnen@marnen.org>
2018-03-07 00:10:16 -0500

    Configure test.watch to run ESpec and white_bread. [#70]

--- phoenix/contraq/config/dev.exs
+++ phoenix/contraq/config/dev.exs
@@ -44,6 +44,8 @@ config :contraq, ContraqWeb.Endpoint,
 # Do not include metadata nor timestamps in development logs
 config :logger, :console, format: "[$level] $message\n"
 
+config :mix_test_watch, tasks: ["espec", "white_bread.run"]
+
 # Set a higher stacktrace during development. Avoid configuring such
 # in production as building large stacktraces may be expensive.
 config :phoenix, :stacktrace_depth, 20
