61919bfae0e4ac74841a35b15bac578eb1dd2ab7
Marnen Laibow-Koser <marnen@marnen.org>
2018-03-09 12:22:08 -0500

    Comment out code that we’re not using yet. [#70]

--- phoenix/contraq/lib/contraq_web/controllers/gig_controller.ex
+++ phoenix/contraq/lib/contraq_web/controllers/gig_controller.ex
@@ -14,47 +14,47 @@ defmodule ContraqWeb.GigController do
     render(conn, "new.html", changeset: changeset)
   end
 
-  def create(conn, %{"gig" => gig_params}) do
-    case Gigs.create_gig(gig_params) do
-      {:ok, gig} ->
-        conn
-        |> put_flash(:info, "Gig created successfully.")
-        |> redirect(to: gig_path(conn, :show, gig))
-      {:error, %Ecto.Changeset{} = changeset} ->
-        render(conn, "new.html", changeset: changeset)
-    end
-  end
-
-  def show(conn, %{"id" => id}) do
-    gig = Gigs.get_gig!(id)
-    render(conn, "show.html", gig: gig)
-  end
-
-  def edit(conn, %{"id" => id}) do
-    gig = Gigs.get_gig!(id)
-    changeset = Gigs.change_gig(gig)
-    render(conn, "edit.html", gig: gig, changeset: changeset)
-  end
-
-  def update(conn, %{"id" => id, "gig" => gig_params}) do
-    gig = Gigs.get_gig!(id)
-
-    case Gigs.update_gig(gig, gig_params) do
-      {:ok, gig} ->
-        conn
-        |> put_flash(:info, "Gig updated successfully.")
-        |> redirect(to: gig_path(conn, :show, gig))
-      {:error, %Ecto.Changeset{} = changeset} ->
-        render(conn, "edit.html", gig: gig, changeset: changeset)
-    end
-  end
-
-  def delete(conn, %{"id" => id}) do
-    gig = Gigs.get_gig!(id)
-    {:ok, _gig} = Gigs.delete_gig(gig)
-
-    conn
-    |> put_flash(:info, "Gig deleted successfully.")
-    |> redirect(to: gig_path(conn, :index))
-  end
+  # def create(conn, %{"gig" => gig_params}) do
+  #   case Gigs.create_gig(gig_params) do
+  #     {:ok, gig} ->
+  #       conn
+  #       |> put_flash(:info, "Gig created successfully.")
+  #       |> redirect(to: gig_path(conn, :show, gig))
+  #     {:error, %Ecto.Changeset{} = changeset} ->
+  #       render(conn, "new.html", changeset: changeset)
+  #   end
+  # end
+  #
+  # def show(conn, %{"id" => id}) do
+  #   gig = Gigs.get_gig!(id)
+  #   render(conn, "show.html", gig: gig)
+  # end
+  #
+  # def edit(conn, %{"id" => id}) do
+  #   gig = Gigs.get_gig!(id)
+  #   changeset = Gigs.change_gig(gig)
+  #   render(conn, "edit.html", gig: gig, changeset: changeset)
+  # end
+  #
+  # def update(conn, %{"id" => id, "gig" => gig_params}) do
+  #   gig = Gigs.get_gig!(id)
+  #
+  #   case Gigs.update_gig(gig, gig_params) do
+  #     {:ok, gig} ->
+  #       conn
+  #       |> put_flash(:info, "Gig updated successfully.")
+  #       |> redirect(to: gig_path(conn, :show, gig))
+  #     {:error, %Ecto.Changeset{} = changeset} ->
+  #       render(conn, "edit.html", gig: gig, changeset: changeset)
+  #   end
+  # end
+  #
+  # def delete(conn, %{"id" => id}) do
+  #   gig = Gigs.get_gig!(id)
+  #   {:ok, _gig} = Gigs.delete_gig(gig)
+  #
+  #   conn
+  #   |> put_flash(:info, "Gig deleted successfully.")
+  #   |> redirect(to: gig_path(conn, :index))
+  # end
 end
